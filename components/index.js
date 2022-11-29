d7124fbb08f3cf600d7d061c6e6c7cf0b8319620
Deolu_Josef <kiishijoseph@gmail.com>
2022-11-29 18:12:55 +0100

    initiated an empty repo, installed expo, initiated expo, installed dependencies......

---
+++ components/index.js
@@ -0,0 +1,24 @@
+import HomeHeader from './HomeHeader';
+
+import FocusStatusBar from './FocusStatusBar';
+
+import NFTCard from './NFTCard';
+
+import { CircleButton, RectButton } from './Button';
+
+import SubInfo from './SubInfo';
+
+import DetailsDesc from './DetailsDesc';
+
+import DetailsBid from './DetailsBid';
+
+export {
+  HomeHeader,
+  FocusStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  DetailsBid,
+  DetailsDesc,
+};
---
+++ screens/Blank.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Blank = () => {
+  return (
+    <View>
+      <Text>Blank</Text>
+    </View>
+  )
+}
+
+export default Blank
